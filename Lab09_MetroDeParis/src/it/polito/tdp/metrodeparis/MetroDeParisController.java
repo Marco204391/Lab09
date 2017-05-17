58844f5198f4cb63b510e95b130b8224f034fa3a
Marco204391 <marco-frenc@hotmail.it>
2017-05-17 11:45:19 +0200

    123

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 
 public class MetroDeParisController {
 	
@@ -19,7 +20,10 @@ public class MetroDeParisController {
 
     @FXML
     private URL location;
-
+    
+    @FXML
+	private TextArea txtRisultato;
+    
     @FXML
     private ComboBox<Fermata> cbxPartenza;
 
@@ -31,12 +35,51 @@ public class MetroDeParisController {
 
     @FXML
     void handleCerca(ActionEvent event) {
+    	
+    	Fermata stazioneDiPartenza = cbxPartenza.getValue();
+		Fermata stazioneDiArrivo = cbxarrivo.getValue();
+
+		if (stazioneDiPartenza != null && stazioneDiArrivo != null) {
+			
+			if (!stazioneDiPartenza.equals(stazioneDiArrivo)) {
+
+				try {
+					// Calcolo il percorso tra le due stazioni
+					model.calcolaPercorso(stazioneDiPartenza, stazioneDiArrivo);
+					
+					// Ottengo il tempo di percorrenza
+					int tempoTotaleInSecondi = (int) model.getPercorsoTempoTotale();
+					int ore = tempoTotaleInSecondi / 3600;
+					int minuti = (tempoTotaleInSecondi % 3600) / 60;
+					int secondi = tempoTotaleInSecondi % 60;
+					String timeString = String.format("%02d:%02d:%02d", ore, minuti, secondi);
+					StringBuilder risultato = new StringBuilder();
+
+					// Ottengo il percorso
+					risultato.append(model.getPercorsoEdgeList());
+					risultato.append("\n\nTempo di percorrenza stimato: " + timeString + "\n");
+
+					// Aggiorno la TextArea
+					txtRisultato.setText(risultato.toString());
+
+				} catch (RuntimeException e) {
+					txtRisultato.setText(e.getMessage());
+				}
+			
+			} else {
+				txtRisultato.setText("Inserire una stazione di arrivo diversa da quella di partenza.");
+			}
+
+		} else {
+			txtRisultato.setText("Inserire una stazione di arrivo ed una di partenza.");
+		}
 
     }
 
     @FXML
     void initialize() {
-        assert cbxPartenza != null : "fx:id=\"cbxPartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+    	assert txtRisultato != null : "fx:id=\"txtElencoStazioni\" was not injected: check your FXML file 'gui.fxml'.";
+    	assert cbxPartenza != null : "fx:id=\"cbxPartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
         assert cbxarrivo != null : "fx:id=\"cbxarrivo\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
         assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
 
@@ -44,7 +87,7 @@ public class MetroDeParisController {
     
     public void setModel(Model model) {
 		this.model=model;
-		
+		model.createGraph();
 		cbxPartenza.getItems().addAll(model.getFermate());
 		cbxarrivo.getItems().addAll(model.getFermate());
 	}
--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
@@ -3,6 +3,7 @@ package it.polito.tdp.metrodeparis.model;
 import java.util.List;
 
 import org.jgrapht.Graphs;
+import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 import com.javadocmd.simplelatlng.LatLngTool;
@@ -16,6 +17,9 @@ public class Model {
 	private List<Fermata> fermate;
 	private List<Linea> linee;
 	private List<Connessione> connessioni;
+	
+	private List<DefaultWeightedEdge> pathEdgeList = null;
+	private double pathTempoTotale = 0;
 	
 	public List<Fermata> getFermate(){
 		if(this.fermate==null){
@@ -48,4 +52,44 @@ public class Model {
 		System.out.println("Grafo creato : numero di vertici "+ graph.vertexSet().size()+ " e numero archi " + graph.edgeSet().size());
 	}
 	
+	
+	public void calcolaPercorso(Fermata partenza, Fermata arrivo){
+		
+		DijkstraShortestPath<Fermata, DefaultWeightedEdge> dijkstra = new DijkstraShortestPath<Fermata, DefaultWeightedEdge>(graph, partenza, arrivo);
+		
+		pathEdgeList = dijkstra.getPathEdgeList();
+		pathTempoTotale = dijkstra.getPathLength();
+		
+		if (pathEdgeList.size() - 1 > 0) {
+			pathTempoTotale += (pathEdgeList.size() - 1) * 30;
+		}
+	}
+	public String getPercorsoEdgeList() {
+
+		if (pathEdgeList == null)
+			throw new RuntimeException("Non è stato creato nessun percorso.");
+
+
+		StringBuilder risultato = new StringBuilder();
+		risultato.append("Percorso: [ ");
+
+		for (DefaultWeightedEdge edge : pathEdgeList) {
+			risultato.append(graph.getEdgeTarget(edge).getNome());
+			risultato.append(", ");
+		}
+
+		risultato.setLength(risultato.length()-2);
+		risultato.append("]");
+		
+		return risultato.toString();
+
+	}
+	
+	public double getPercorsoTempoTotale() {
+
+		if (pathEdgeList == null)
+			throw new RuntimeException("Non è stato creato nessun percorso.");
+		return pathTempoTotale;
+
+	}
 }
